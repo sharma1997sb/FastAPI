f2dd1802445cb061b5326e253cf9b7c016bcbd16
sharma1997sb <sh.sharma@shiftboolean.com>
2022-02-11 13:45:59 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, status
+from database import Base, engine
+from pydantic import BaseModel
+import uvicorn
+
+# Create ToDoRequest Base Model
+class ToDoRequest(BaseModel):
+    task: str
+
+# Create the database
+Base.metadata.create_all(engine)
+
+# Initialize app
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return "todooo"
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+def create_todo():
+    return "create todo item"
+
+@app.get("/todo/{id}")
+def read_todo(id: int):
+    return "read todo item with id {id}"
+
+@app.put("/todo/{id}")
+def update_todo(id: int):
+    return "update todo item with id {id}"
+
+@app.delete("/todo/{id}")
+def delete_todo(id: int):
+    return "delete todo item with id {id}"
+
+@app.get("/todo")
+def read_todo_list():
+    return "read todo list"
